90ff4258b960547bc5cf29535ac529350efb5998
manuayalag <csmanuel36@gmail.com>
2025-04-11 15:50:58 -0300

    Agregue componente RoomCard para listar habitaciones

--- hotel-admin/src/App.jsx
+++ hotel-admin/src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home.page"
 import Rooms from "./pages/Rooms.page"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/Home.page"
 
 function App() {
 
