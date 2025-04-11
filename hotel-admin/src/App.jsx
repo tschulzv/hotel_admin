ed3bd45e980d87895d6de76fe2b2d2c64eb0a1a4
Tania Schulz <taniaschulzv@gmail.com>
2025-04-11 14:38:37 -0300

    initial commit

---
+++ hotel-admin/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+
+function App() {
+
+  return (
+    <Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
+  </Router>
+  )
+}
+
+export default App
