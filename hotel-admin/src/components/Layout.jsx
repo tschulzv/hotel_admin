e65027835945c6e29d72f41235c65f47c5ff9d52
Tania Schulz <taniaschulzv@gmail.com>
2025-04-11 16:20:04 -0300

    added paginated table

--- hotel-admin/src/components/Layout.jsx
+++ hotel-admin/src/components/Layout.jsx
@@ -5,7 +5,7 @@ import {Container} from 'react-bootstrap';
 const Layout = ({children}) => {
   return (
     <div className="d-flex min-vh-100">
-      <div className="min-vh-100" style={{ width: "250px", height: "100%"}}>
+      <div className="min-vh-100 h-100" style={{ width: "250px"}}>
         <Navigation />
       </div>
 
