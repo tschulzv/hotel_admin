ed5000a78fa7e0e47f859ab04e0609947b9fe377
Otto Enzler <enzler.otto@gmail.com>
2025-04-22 16:08:51 -0300

    arregle problemas con el estilo del login y el layout

--- hotel-admin/src/components/Layout.jsx
+++ hotel-admin/src/components/Layout.jsx
@@ -1,30 +1,25 @@
-import React from 'react'
-import Navigation from './Navigation'
-import {Container} from 'react-bootstrap';
-import '../layout.css';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import Navigation from "./Navigation";
+import { Container } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
-const Layout = ({children}) => {
-  // PARA EL LOGIN
-  const location = useLocation();
-  const showNavbar = location.pathname !== "/";
+const Layout = ({ children }) => {
   return (
     <div className="d-flex min-vh-100">
-      {/* CAMBIE ESTO PARA EL LOGIN*/}
-      {showNavbar && (
-        <div className="min-vh-100 h-100" style={{ width: "250px" }}>
-          <div className="sidebar min-vh-100" style={{ width: "200px", height: "100%" }}>
-            <Navigation />
-          </div>
+      <div className="min-vh-100 h-100" style={{ width: "250px" }}>
+        <div
+          className="sidebar min-vh-100"
+          style={{ width: "200px", height: "100%" }}
+        >
+          <Navigation />
         </div>
-      )}
+      </div>
+
       <main className="page-content flex-grow-1 w-100">
-        <Container fluid>
-          {children}
-        </Container>
+        <Container fluid>{children}</Container>
       </main>
     </div>
   );
-}
+};
 
 export default Layout;
